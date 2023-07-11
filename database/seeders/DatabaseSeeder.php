81f4f3cdcad83a95840b5b166089e85b2a26530d
南博之伸 <minamihironoshin@minamihiroyukishinnoMacBook-Pro.local>
2023-07-12 04:07:54 +0900

    shop追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call([AdminSeeder::class, OwnerSeeder::class ]);
+        $this->call([
+            AdminSeeder::class, 
+            OwnerSeeder::class, 
+            ShopSeeder::class,
+        ]);
      }
 }
